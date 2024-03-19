2e0df38fe6b8d1b0e2b1323ee794f05ae021245b
mustafatavasli <musstafatavasli@gmail.com>
2024-03-19 11:44:25 +0300

    Initial Commit

---
+++ PokemonWidgetKit/PokemonWidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidgetKitApp.swift
+//  PokemonWidgetKit
+//
+//  Created by Mustafa TAVASLI on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
